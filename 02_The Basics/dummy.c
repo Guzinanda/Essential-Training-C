cb1f99c07191fa441b2dccada5df72206e7052ac
Fernanda Guzman Gomez <fernandamac@Fernandas-MacBook-Pro.local>
2020-12-05 15:29:31 -0600

    01. The Basics

---
+++ 02_The Basics/dummy.c
@@ -0,0 +1,19 @@
+
+/* Fernanda Guzman Dic 01 2020*/
+
+#include <stdio.h>
+
+int main() // A dummy is just this (minimmum to compile)
+{
+    // Just display strings and adds a \n at the end.
+    puts("I'am a computer!");
+    puts("Thrilled to meet you!");
+
+    // printf is more powerfull than puts
+    // it display integer, float, characterm hexa, etc.
+    // it does not ad a \n at the end.
+    printf("This is the way the world ends");
+    printf("Not with a bang but a whimper");
+
+    return(0);
+}
